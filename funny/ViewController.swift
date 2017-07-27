09164698c561e5f6f5f0dd3203a6478ab15c717e
422Mac01 <422mac01@422Mac01s-iMac.local>
2017-07-27 16:08:32 +0700

    Initial Commit

---
+++ funny/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  funny
+//
+//  Created by 422Mac01 on 27/7/60.
+//  Copyright © พ.ศ. 2560 jane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
