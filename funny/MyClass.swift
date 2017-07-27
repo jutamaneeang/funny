a09a9ad79c67b4d51117851440adbfd6962e2bd3
422Mac01 <422mac01@422Mac01s-iMac.local>
2017-07-27 16:38:59 +0700

    create class

---
+++ funny/MyClass.swift
@@ -0,0 +1,18 @@
+//
+//  MyClass.swift
+//  funny
+//
+//  Created by 422Mac01 on 27/7/60.
+//  Copyright © พ.ศ. 2560 jane. All rights reserved.
+//
+
+import Foundation
+
+
+class MyClass {
+    
+    let strName = "Doramon"
+    
+    
+    
+} //MyClass
